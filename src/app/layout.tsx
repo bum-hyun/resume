96d630b804f4c00c54239f9eb1f9a6996bb709a5
fridgecrew-bumhyun <loel@fridgecrew.com>
2025-01-04 21:52:10 +0900

    feature: 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,25 @@
 import './globals.css';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 
 export const metadata: Metadata = {
   title: '이력서 - 김범현',
   description: '프론트엔드 개발자 김범현입니다.',
+  openGraph: {
+    title: '이력서 - 김범현',
+    type: 'website',
+    url: 'https://bum-hyun.github.io/resume',
+    image: '/resume/profile.jpeg',
+    description: '프론트엔드 개발자 김범현입니다.',
+    locale: 'ko_KR',
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 interface RootLayoutProps {
