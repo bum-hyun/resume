ce36653881b0bbffb22b059685438e1fa0272b01
fridgecrew-bumhyun <loel@fridgecrew.com>
2025-01-04 15:55:27 +0900

    feature: next 이식

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/resume',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
